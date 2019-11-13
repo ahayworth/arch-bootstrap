3b82c1f08d33d8a097aadc83c513eda857b85a1f
Andrew Hayworth <ahayworth@gmail.com>
2019-11-13 15:09:32 -0600

    add some crap

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,6 +47,8 @@ if [[ "$1" == "--stage-one" ]]; then
     mkpart primary linux-swap 551MiB 30518MiB name 2 swap \
     mkpart primary ext4 30518MiB 100% name 3 root
 
+  sfdisk --part-type ${DISK} 3 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
+
   mkfs.fat -F32 -n boot ${DISK}p1
   mkswap -L swap ${DISK}p2
   swapon ${DISK}p2
@@ -86,13 +88,12 @@ EOF
 MODULES=(i915 battery)
 BINARIES=()
 FILES=()
-HOOKS=(systemd autodetect block filesystems modconf sd-vconsole keyboard)
+HOOKS=(systemd sd-plymouth autodetect block filesystems modconf sd-vconsole keyboard fsck)
 COMPRESSION="cat"
 EOF
 
   efibootmgr --disk ${DISK} --part 1 --create --label 'Arch Linux' --loader /vmlinuz-linux --verbose \
-    --unicode 'root=/dev/${DISK}p3 rw quiet loglevel=3 rd.systemd.show_status=auto rd.udev.log_priority=3 i915.fastboot=1 vga=current initrd=\intel-ucode.img initrd=\initramfs-linux.img'
-    #--unicode "root=${DISK}p3 rw initrd=\intel-ucode.img initrd=\initramfs-linux.img rd.driver.blacklist=nouveau rd.luks=0 rd.lvm=0 rd.md=0 rd.dm=0"
+    --unicode "root=/dev/${DISK}p3 rw quiet loglevel=0 rd.systemd.show_status=auto rd.udev.log_priority=3 i915.fastboot=1 vga=current initrd=\intel-ucode.img initrd=\initramfs-linux.img"
 
   ln -sf /usr/bin/vim /usr/bin/vi
   useradd -m -u 1000 andrew
@@ -106,28 +107,20 @@ EOF
   echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 elif [[ "$1" == "--stage-three" ]]; then
   pacman -S --noconfirm gnome gnome-extra
-  # TODO: pacdep from aur
   pacman -S --noconfirm --asdeps bash-completion reflector
-  # TODO: consider fwupd, gst plugins, graphics and acceleration, power and thermal management
-  # do i need samba client support any more?
-  # gnome boxes (qemu, look at libvit dependencies)
-  #  also: battery module
-  #  also: pacman hook for update
-  # wiki page for x1 extreme
   systemctl enable gdm
-  # configure gdm
-  pacman -S --noconfirm s-tui throttled htop cpupower
-  systemctl enable lenovo_fix
+  pacman -S --noconfirm s-tui htop cpupower
   sed -i'' -e "s/#governor='ondemand'/governor='performance'/" /etc/default/cpupower
   systemctl enable cpupower
 
-  echo 'options psmouse synaptics_intertouch=1' > /etc/modprobe.d/psmouse.conf
   systemctl enable bluetooth
   systemctl enable NetworkManager
 
   pacman -S --noconfirm vulkan-intel vulkan-icd-loader intel-media-driver libva-utils vulkan-tools
   echo 'options i915 enable_guc=3 enable_fbc=1' > /etc/modprobe.d/i915.conf
   mkinitcpio -p linux
+
+  echo 'options snd_hda_intel power_save=1' > /etc/modprobe.d/audio_powersave.conf
 
   mkdir -p ~andrew/.config/environment.d
   echo 'LIBVA_DRIVER_NAME=iHD' >> ~andrew/.config/environment.d/envvars.conf
@@ -149,6 +142,213 @@ Include = /etc/pacman.d/mirrorlist
 EOF
 
   pacman -Sy
-  pacman -S steam
+  pacman -S steam lib32-primus_vk
   yay -S slack-desktop
+
+  mkdir -p /etc/dconf/profile
+  cat >> /etc/dconf/profile/gdm <<EOF
+user-db:user
+system-db:gdm
+file-db:/usr/share/gdm/greeter-dconf-defaults
+EOF
+
+  mkdir -p /etc/dconf/db/gdm.d
+  cat >> /etc/dconf/db/gdm.d/02-clock <<EOF
+[org/gnome/desktop/interface]
+clock-format='12h'
+EOF
+  dconf update
+  pacman -S adapta-gtk-theme arc-gtk-theme arc-icon-theme gtk-engine-murrine noto-fonts ttf-roboto
+
+  pacman -S powertop gnome-software-packagekit-plugin gnome-power-manager
+
+  cat >> /etc/systemd/system/powertop.service <<EOF
+[Unit]
+Description=Powertop tunings
+
+[Service]
+Type=exec
+ExecStart=/usr/bin/powertop --auto-tune
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  systemctl daemon-reload
+  systemctl enable powertop
+
+  pacman -S lib32-libpulse lib32-alsa-plugins pulseeffects
+  curl -L -o '/home/andrew/.config/PulseEffects/irs/Dolby ATMOS ((128K MP3)) 1.Default.irs' 'https://github.com/JackHack96/PulseEffects-Presets/raw/master/irs/Dolby%20ATMOS%20((128K%20MP3))%201.Default.irs'
+  yay -S thermald dptfxtract-bin
+  dptfxtract -o /etc/thermald
+  mv thermal-conf* /etc/thermald/
+  systemctl enable --now thermald
+
+  yay -S intel-undervolt
+  sed -i'' -e "s/undervolt 0 'CPU' 0/undervolt 0 'CPU' -100" /etc/intel-undervolt.conf
+  sed -i'' -e "s/undervolt 0 'CPU Cache' 0/undervolt 0 'CPU Cache' -100" /etc/intel-undervolt.conf
+  systemctl enable --now intel-undervolt
+
+  pacman -S cups hplip python-pyqt5
+  yay -S hplip-plugin
+  systemctl enable --now org.cups.cupsd.socket
+  systemctl enable --now org.cups.cupsd.service
+
+  systemctl enable fstrim.timer
+
+  #pacman -S alacritty alacritty-terminfo
+  yay -S nerd-fonts-hack alacritty-git alacritty-terminfo-git
+
+  pacman -S zsh
+  chsh -s /usr/bin/zsh andrew
+
+  gsettings set org.gnome.settings-daemon.plugins.media-keys \
+    area-screenshot \
+    "['<Shift><Alt>4']"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys \
+    area-screenshot-clip \
+    "['<Ctrl><Shift><Alt>4']"
+
+  gsettings set org.gnome.settings-daemon.plugins.media-keys \
+    screenshot \
+    "['<Shift><Alt>5']"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys \
+    screenshot-clip \
+    "['<Ctrl><Shift><Alt>5']"
+
+  gsettings set org.gnome.settings-daemon.plugins.media-keys \
+    window-screenshot \
+    "['<Shift><Alt>6']"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys \
+    window-screenshot-clip \
+    "['<Ctrl><Shift><Alt>6']"
+
+  gsettings set org.gnome.desktop.input-sources \
+    xkb-options \
+    "['compose:prsc','caps:escape']"
+
+  gsettings set org.gnome.desktop.interface \
+    show-battery-percentage \
+    "true"
+
+  yay -S firefox-nightly
+  yay -S chromium-vaapi-bin
+  yay -S chromium-widevine
+  cat > ~/.config/chromium-flags.conf << EOF
+--ignore-gpu-blacklist
+--enable-gpu-rasterization
+--enable-oop-rasterization
+--enable-native-gpu-memory-buffers
+--enable-zero-copy
+--use-gl=egl
+--enable-features=UseSkiaRenderer,VizHitTestSurfaceLayer
+EOF
+
+  pacman -S nvme-cli intel-gpu-tools bluez-utils
+  usermod -G lp -a andrew
+  mkdir -p /var/lib/gdm/.config/pulse
+  cat > /var/lib/gdm/.config/pulse/client.conf <<EOF
+autospawn = no
+daemon-binary = /bin/true
+EOF
+  chown gdm:gdm /var/lib/gdm/.config/pulse/client.conf
+
+  yay -S nordic-theme-git
+
+  echo 'vm.dirty_writeback_centisecs = 3000' > /etc/sysctl.d/writeback.conf
+  echo 'options iwlwifi power_save=1 uapsd_disable=0' > /etc/modprobe.d/iwlwifi.conf
+
+  pacman -S linux-zen
+  pacman -Rnu bbswitch
+  pacman -S linux-zen-headers
+  yay -S bbswitch-dkms
+  num=`efibootmgr | grep Arch | awk '{print $1}' | sed -E -e 's/(Boot|\*)//g'`
+  efibootmgr -b $num -B
+  efibootmgr --disk ${DISK} --part 1 --create --label 'Arch Linux' --loader /vmlinuz-linux-zen --verbose \
+    --unicode "root=/dev/${DISK}p3 rw quiet splash i915.fastboot=1 loglevel=3 rd.udev.log_priority=3 rd.systemd.show_status=auto vga=current initrd=\intel-ucode.img initrd=\initramfs-linux-zen.img"
+
+
+  yay -S plymouth-git gdm-plymouth ttf-dejavu
+  systemctl disable gdm
+  systemctl enable gdm-plymouth
+
+  curl -L -o /tmp/uefi-shell.zip 'https://github.com/tianocore/edk2/releases/download/edk2-stable201908/ShellBinPkg.zip'
+  unzip /tmp/uefi-shell.zip -d /tmp/uefi-shell
+  cp /tmp/uefi-shell/ShellBinPkg/UefiShell/X64/Shell.efi /boot/
+  rm -rf /tmp/uefi-shell
+  efibootmgr --disk ${DISK} --part 1 --create --label 'UEFI Shell' --loader /Shell.efi --verbose
+  efibootmgr -o 0001,0000
+
+  yay -S ttf-symbola p7zip
+  unzip windows.zip -d /usr/share/fonts
+  rm -rf /usr/share/fonts/windows/.NET*
+  fc-cache -f
+
+  yay -S visual-studio-code-bin
+
+  yay -S pam-python python-face_recognition python-face_recognition_models
+  (cd howdy && makepkg -si)
+
+  yay -S chrome-gnome-shell
+
+  pacman -S docker docker-compose
+  usermod -G docker -a andrew
+
+  pacman -S lm_sensors
+  pacman -S libgtop lm_sensors gnome-icon-theme-symbolic
+
+  gpg --recv-key 06CA9F5D1DCF2659
+  yay -S ofono phonesim
+  systemctl enable --now ofono
+
+  pacman -Ss ansible python-passlib sshpass
+  cat >> /etc/NetworkManager/conf.d/wifi_backend.conf <<EOF
+[device]
+wifi.backend=iwd
+EOF
+  systemctl enable --now iwd
+  systemctl restart NetworkManager
+
+  pacman -S dbus-broker
+  systemctl enable --now dbus-broker
+  systemctl enable --global dbus-broker
+
+  pacman -S systemd-resolvconf
+  systemctl enable --now systemd-resolved
+  ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+  cat >> /etc/NetworkManager/conf.d/mdns.conf <<EOF
+[connection]
+connection.mdns=2
+connection.llmnr=2
+EOF
+
+
+  # TODO: gst plugins, power and thermal management
+  # TODO: go through power management page again, look into usb autosuspend, sata autosuspend, pcie autosuspend
+  # TODO: zswap?
+  # gnome boxes (qemu, look at libvit dependencies)
+  # wiki page for x1 extreme
+  # configure gdm
+  # export pulseeffects profiles?
+  # export favorites for gnome shell?
+  # fonts
+  # rebuild packages for -march=<foo>
+  # themes
+  # figure out PCIE ASPM
+  # figure out if we want to early-adopt iwd for networkmanager backend
+
+  # FAR-off TODO:
+  # caps -> esc in console
+  # configure tmux/nord: https://github.com/arcticicestudio/nord-tmux/tree/v0.3.0/src
+  # configure vim/nord: https://github.com/arcticicestudio/nord-vim
+  # thinkpad things:
+  # thinkpad-wmi
+  # tp_smapi
+  # tpacpi-bat
+  # threshy
+  # tp-battery-mode
+  # thinkalert
+  # thinkfan
+  # thinkgui
 fi
